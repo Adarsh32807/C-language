5e5d2516d3687a552a08d14b3d259d623cbc24ef
Adarsh32807 <dadarshd07@gmail.com>
2024-08-05 11:11:44 +0530

    Add files via upload

---
+++ loop/Day 9 Q 4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n;
+	printf("enter n =");
+	scanf("%d",&n);
+	
+	while(n>=1)
+	{
+		if(n%2==1)
+		{
+		printf("%d\n",n);	
+			
+			
+		}
+		n--;
+	}
+	
+}
---
+++ loop/Day 9 Q 5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n;
+	int a;
+	printf("enter n =");
+	scanf("%d",&a);
+	
+	printf("enter n =");
+	scanf("%d",&n);
+	
+	while(a<=n)
+	{
+		if(a%4==0)
+		{
+		printf("%d\n",a);	
+			
+			
+		}
+		a++;
+	}
+	
+}
