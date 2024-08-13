9504bf2bc5dba37a5225d4894e9cfc06607c0039
Adarsh32807 <dadarshd07@gmail.com>
2024-08-13 11:35:51 +0530

    Add files via upload

---
+++ Pattern/Q 10.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+ main()
+{
+	int i,j,num=1;
+	
+		for(i=5; i<=1;i++)
+	{
+		for(j=1; j<=i;j++)
+		{
+			printf("%d",num);
+			num++;
+		}
+		printf("\n");
+	}
+}
