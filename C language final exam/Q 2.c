1e167766d2bf685bcd6420a317ecdf5d8742ea13
Adarsh32807 <dadarshd07@gmail.com>
2024-09-21 09:59:26 +0530

    Add files via upload

---
+++ C language final exam/Q 2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+// wap to find a max number from 3 numbers.
+
+void main()
+{
+		int x,y,z;
+		
+		printf("enter value x");
+		scanf("%d",&x);
+		
+		printf("enter value y");
+		scanf("%d",&y);
+		
+		printf("enter value z");
+		scanf("%d",&z);
+		
+}
---
+++ C language final exam/Q 3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	// Write below pattern using nested loop.
+
+	int i,j;
+	for(i=1;j>=5;i++)
+	{
+		for(j=1;j<=1;j++)
+		{
+			printf("%d",i);
+		}
+		printf("\n");
+	}
+}
